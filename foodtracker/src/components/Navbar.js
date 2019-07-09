3bd01808d437a36372d41c5c41bc8854681bbe8e
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 13:45:25 -0700

    Add ability to consume multiple contexts

--- foodtracker/src/components/Navbar.js
+++ foodtracker/src/components/Navbar.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
   render() {
     return ( 
       // another way for this component to consume a context
       // can be used in a functional component with multiple contexts
-      <ThemeContext.Consumer>{(context) => {
-        // setting properties in this component to match ones in context
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return(
-          <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Context app</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact us</li>
-            </ul>
-          </nav>
-        )
-      }}</ThemeContext.Consumer>
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          // setting properties in this component to match ones in context
+          const { isAuthenticated, toggleAuth } = authContext;
+          const { isLightTheme, light, dark } = themeContext;
+          const theme = isLightTheme ? light : dark;
+          return(
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context app</h1>
+              <div onClick={toggleAuth}>
+                { isAuthenticated ? "Logged in" : "Logged out" }
+              </div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact us</li>
+              </ul>
+            </nav>
+          )
+        }}</ThemeContext.Consumer>
+      )}
+      </AuthContext.Consumer>
     );
   }
 }
