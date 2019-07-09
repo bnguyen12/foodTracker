09f0e77691144c2b0db23ca0e5bee9cfb5c19709
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 13:25:02 -0700

    Add new approach for Navbar to consume context

--- foodtracker/src/components/Navbar.js
+++ foodtracker/src/components/Navbar.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  // attaches state of ThemeContextProvider into this component
-  static contextType = ThemeContext;
   render() {
-    console.log(this.context);
-    
-    // setting properties in this component to match ones in context
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return ( 
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context app</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact us</li>
-        </ul>
-      </nav>
+      // another way for this component to consume a context
+      // can be used in a functional component with multiple contexts
+      <ThemeContext.Consumer>{(context) => {
+        // setting properties in this component to match ones in context
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return(
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context app</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact us</li>
+            </ul>
+          </nav>
+        )
+      }}</ThemeContext.Consumer>
     );
   }
 }
