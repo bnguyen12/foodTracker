c51eec18e10b38adba56ae7340de5049c6ca387e
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 13:39:41 -0700

    Made auth context

--- foodtracker/src/App.js
+++ foodtracker/src/App.js
@@ -4,14 +4,17 @@ import Navbar from './components/Navbar';
 import FoodList from './components/FoodList';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar/>
-        <FoodList/>
-        <ThemeToggle/>
+        <AuthContextProvider>
+          <Navbar/>
+          <FoodList/>
+          <ThemeToggle/>
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
