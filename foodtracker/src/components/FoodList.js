8302e6c9fac5b07041e9ca0862234625e16c2081
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 16:04:20 -0700

    Change from using context only to using context with hooks

--- foodtracker/src/components/FoodList.js
+++ foodtracker/src/components/FoodList.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class FoodList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return ( 
-      <div className='food-list' style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }} >Eggs</li>
-          <li style={{ background: theme.ui }}>Rice</li>
-          <li style={{ background: theme.ui }}>Protein bar</li>
-        </ul>
-      </div>
-    );
-  }
+// Using only context API in a component
+// class FoodList extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return ( 
+//       <div className='food-list' style={{ color: theme.syntax, background: theme.bg }}>
+//         <ul>
+//           <li style={{ background: theme.ui }} >Eggs</li>
+//           <li style={{ background: theme.ui }}>Rice</li>
+//           <li style={{ background: theme.ui }}>Protein bar</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// Using context and hooks inside of a functional component
+const FoodList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className='food-list' style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }} >Eggs</li>
+        <li style={{ background: theme.ui }}>Rice</li>
+        <li style={{ background: theme.ui }}>Protein bar</li>
+      </ul>
+    </div>
+  );
 }
 
 export default FoodList;
