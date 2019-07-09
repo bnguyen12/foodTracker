1063f17d239d218e93810f9e59fc7453a4415d4c
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 15:58:29 -0700

    Add demo usage of useEffect hook

--- foodtrackerhooksonly/src/components/FoodList.js
+++ foodtrackerhooksonly/src/components/FoodList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import uuid from 'uuid/v1';
 import NewFoodForm from './NewFoodForm';
@@ -11,11 +11,23 @@ const FoodList = () => {
     { name: 'bread', id: 3 }
   ]);
 
+  const [age, setAge] = useState(20);
+
   const addFood = (name) => {
     // completely replaces the current value of "foods"
     // uuid creates a unique id when invoked
     setFoods([...foods, { name, id: uuid() }]);
   }
+
+  // runs every time component re-renders
+  // only calls this function when data inside the array arg is changed
+  useEffect(() => {
+    console.log('useEffect hook ran', foods);
+  }, [foods])
+
+  useEffect(() => {
+    console.log('useEffect hook ran', age);
+  }, [age])
 
   return ( 
     <div className='food-list'>
@@ -25,6 +37,7 @@ const FoodList = () => {
         })}
       </ul>
       <NewFoodForm addFood={addFood} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 }
