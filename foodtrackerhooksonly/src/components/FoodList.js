2cf1742ab4a25c96115bf4559d81c82ea5b85cdb
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 14:07:17 -0700

    Implement useState hook within a functional component

--- foodtrackerhooksonly/src/components/FoodList.js
+++ foodtrackerhooksonly/src/components/FoodList.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const FoodList = () => {
+  // foods is the data, setFoods is the function to change data
+  const [foods, setFoods] = useState([
+    { name: 'jelly', id: 1 },
+    { name: 'peanut butter', id: 2 },
+    { name: 'bread', id: 3 }
+  ]);
+
+  const addFood = () => {
+    // completely replaces the current value of "foods"
+    // uuid creates a unique id when invoked
+    setFoods([...foods, { name: 'new food', id: uuid() }]);
+  }
+
   return ( 
     <div className='food-list'>
       <ul>
-        <li>peanut butter</li>
-        <li>jelly</li>
-        <li>bread</li>
+        {foods.map(food => {
+          return ( <li key={food.id}>{food.name}</li> )
+        })}
       </ul>
+      <button onClick={addFood}>Add a food</button>
     </div>
   );
 }
