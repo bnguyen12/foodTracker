2e671f41a71367f6bd5075e7b17770266a21f38d
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 13:54:42 -0700

    Made new app for demoing React Hooks only

---
+++ foodtrackerhooksonly/src/components/FoodList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FoodList = () => {
+  return ( 
+    <div className='food-list'>
+      <ul>
+        <li>peanut butter</li>
+        <li>jelly</li>
+        <li>bread</li>
+      </ul>
+    </div>
+  );
+}
+
+export default FoodList;
