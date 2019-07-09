1fe6bdd725516eb8e61ea339cba925b8feeabed5
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 13:19:47 -0700

    Grant ability to access theme context

--- foodtracker/src/components/FoodList.js
+++ foodtracker/src/components/FoodList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class FoodList extends Component {
-  render() { 
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return ( 
-      <div className='food-list'>
+      <div className='food-list' style={{ color: theme.syntax, background: theme.bg }}>
         <ul>
-          <li>Eggs</li>
-          <li>Rice</li>
-          <li>Protein bar</li>
+          <li style={{ background: theme.ui }} >Eggs</li>
+          <li style={{ background: theme.ui }}>Rice</li>
+          <li style={{ background: theme.ui }}>Protein bar</li>
         </ul>
       </div>
     );
--- foodtracker/src/components/Navbar.js
+++ foodtracker/src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  render() { 
+  // attaches state of ThemeContextProvider into this component
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context);
+    
+    // setting properties in this component to match ones in context
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return ( 
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context app</h1>
         <ul>
           <li>Home</li>
