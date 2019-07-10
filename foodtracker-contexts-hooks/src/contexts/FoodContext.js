5af07591bc3c7577d78ed12857be14e4a2e5e6cc
Benny Nguyen <benny.nguyenx@gmail.com>
2019-07-09 20:08:46 -0700

    Integrate local storage

--- foodtracker-contexts-hooks/src/contexts/FoodContext.js
+++ foodtracker-contexts-hooks/src/contexts/FoodContext.js
@@ -1,11 +1,20 @@
-import React, { createContext, useReducer } from 'react';
-import uuid from 'uuid/v1';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { foodReducer } from '../reducers/foodReducer';
 
 export const FoodContext = createContext();
 
 const FoodContextProvider = (props) => {
-  const [foods, dispatch] = useReducer(foodReducer, []);
+  // the third argument gives a default value
+  const [foods, dispatch] = useReducer(foodReducer, [], () => {
+    const localData = localStorage.getItem('foods');
+    return localData ? JSON.parse(localData) : []
+  });
+
+  // save to local storage whenver foods changes
+  useEffect(() => {
+    localStorage.setItem('foods', JSON.stringify(foods));
+  }, [foods]);
+
   return (
     <FoodContext.Provider value={{foods, dispatch}}>
       { props.children }
